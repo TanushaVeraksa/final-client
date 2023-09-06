e95caeb38b85b354ea60d730193674ecf93106a9
TanushaVeraksa <veraksa161@gmail.com>
2023-09-06 09:49:45 +0300

    change style in CustomBody

--- src/components/CustomBody.js
+++ src/components/CustomBody.js
@@ -4,15 +4,15 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 function CustomBody() {
   return (
-    <Card bg='light' className='px-5 pb-3 m-auto'>
-        <CloudUploadIcon className='m-auto' color="primary" sx={{ fontSize: '8rem' }}/>
-            <Card.Text className='m-auto'>
+    <Card bg='light' className='px-5 pb-2 m-auto' style={{border: '4px dotted gray'}}>
+        <CloudUploadIcon className='m-auto' color="primary" sx={{ fontSize: '5rem' }}/>
+            <Card.Text style={{fontSize: '0.8rem'}} className='m-auto'>
                 Drag and Drop Files Here
             </Card.Text>
-            <Card.Text className='m-auto'>
+            <Card.Text style={{fontSize: '0.8rem'}} className='m-auto'>
                 Or
             </Card.Text>
-            <Card.Text className='m-auto'>
+            <Card.Text style={{fontSize: '0.8rem'}} className='m-auto'>
                 Select Files
             </Card.Text>
     </Card>
